666bad3e18f67b7763b4086af4489e4eff6b8876
ws0671 <ws0671@naver.com>
2021-12-14 18:25:06 +0900

    server.js modified

--- src/server.js
+++ src/server.js
@@ -30,6 +30,14 @@ app.use((req, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
