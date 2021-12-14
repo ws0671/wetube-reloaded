6344f734220f3bf99a69e4f677db754eb533e418
ws0671 <ws0671@naver.com>
2021-12-14 15:06:33 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -162,11 +162,11 @@ export const postEdit = async (req, res) => {
       errorMessage: "This username/email is alreay taken.",
     });
   }
-
+  const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,9 +1,33 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const s3ImageUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-ws0671/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-ws0671/videos",
+  acl: "public-read",
+});
+
+const isHeroku = process.env.NODE_ENV === "production";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -30,10 +54,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
